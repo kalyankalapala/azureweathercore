1ec7427ac5716426eba1f7e8681faafd15a7ade1
kalyankalapala <kalyankalapala24@gmail.com>
2021-08-06 18:57:58 +0530

    Add project files.

---
+++ frontend/Pages/WeatherClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace frontend
+{
+    public class WeatherClient
+    {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        private readonly HttpClient httpClient;
+
+        public WeatherClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<WeatherForecast[]> GetWeathersAsync()
+        {
+            var responseMessage = await this.httpClient.GetAsync("/weatherforecast");
+            var stream = await responseMessage.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<WeatherForecast[]>(stream, options);
+        }
+    }
+}
